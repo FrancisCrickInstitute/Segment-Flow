5ead7f839f2e8e8b1aa649e526826586be3c9d64
sea-shunned <seashunned@gmail.com>
2024-02-26 17:12:53 +0000

    Rename patch/tile to avoid confusion

--- modules/models/resources/usr/bin/create_splits.py
+++ modules/models/resources/usr/bin/create_splits.py
@@ -5,16 +5,17 @@ import numpy as np
 import pandas as pd
 
 # TODO: These constants vary by GPU/available memory, and also scale by number of channels which is not considered here
-MAX_TILE_SIZE = {
+# Named substack to avoid patch/tile confusion, and to avoid chunk confusion
+MAX_SUBSTACK_SIZE = {
     "height": 3000,
     "width": 3000,
     "depth": 100,
 }
 
 
-def auto_tile_size(image_shape: tuple[int, ...], num_tiles: tuple[int, ...]):
+def auto_substack_size(image_shape: tuple[int, ...], num_substacks: tuple[int, ...]):
     """
-    Calculate the number of tiles to use for a given image size, for any dimension specified as 'auto'.
+    Calculate the number of substacks to use for a given image size, for any dimension specified as 'auto'.
 
     Assumes the image shape is a tuple of integers representing the dimensions of the image (e.g. (H, W, D)).
 
@@ -23,58 +24,60 @@ def auto_tile_size(image_shape: tuple[int, ...], num_tiles: tuple[int, ...]):
     """
     # Extract the dimensions
     height, width, depth = image_shape
-    # Extract the number of tiles
-    x_tiles, y_tiles, z_tiles = num_tiles
-    if x_tiles == "auto":
-        x_tiles = (height // MAX_TILE_SIZE["height"]) + 1
+    # Extract the number of substacks
+    x_substacks, y_substacks, z_substacks = num_substacks
+    if x_substacks == "auto":
+        x_substacks = (height // MAX_SUBSTACK_SIZE["height"]) + 1
     else:
-        x_tiles = check_sensible_num_tiles(int(x_tiles), height, dim="height")
-    if y_tiles == "auto":
-        y_tiles = (width // MAX_TILE_SIZE["width"]) + 1
+        x_substacks = check_sensible_num_substacks(
+            int(x_substacks), height, dim="height"
+        )
+    if y_substacks == "auto":
+        y_substacks = (width // MAX_SUBSTACK_SIZE["width"]) + 1
     else:
-        y_tiles = check_sensible_num_tiles(int(y_tiles), width, dim="width")
-    if z_tiles == "auto":
-        z_tiles = (depth // MAX_TILE_SIZE["depth"]) + 1
+        y_substacks = check_sensible_num_substacks(int(y_substacks), width, dim="width")
+    if z_substacks == "auto":
+        z_substacks = (depth // MAX_SUBSTACK_SIZE["depth"]) + 1
     else:
-        z_tiles = check_sensible_num_tiles(int(z_tiles), depth, dim="depth")
-    return (x_tiles, y_tiles, z_tiles)
+        z_substacks = check_sensible_num_substacks(int(z_substacks), depth, dim="depth")
+    return (x_substacks, y_substacks, z_substacks)
 
 
-def check_sensible_num_tiles(num_tiles_dim: int, dim_size: int, dim: str):
+def check_sensible_num_substacks(num_stacks_dim: int, dim_size: int, dim: str):
     """
-    Make sure the input number of tiles is sensible for the given dimension size.
+    Make sure the input number of substacks is sensible for the given dimension size.
     """
-    # Make sure the number of tiles is at least 1
-    if num_tiles_dim < 1:
+    # Make sure the number of stackss is at least 1
+    if num_stacks_dim < 1:
         return 1
-    # More tiles than pixels? Use auto method
-    if num_tiles_dim > dim_size:
-        return (dim_size // MAX_TILE_SIZE[dim]) + 1
-    # Make sure it's not too small, defined as 1% of the max tile size
-    if (dim_size // num_tiles_dim) < (MAX_TILE_SIZE[dim] / 100):
+    # More stackss than pixels? Use auto method
+    if num_stacks_dim > dim_size:
+        return (dim_size // MAX_SUBSTACK_SIZE[dim]) + 1
+    # Make sure it's not too small, defined as 1% of the max stacks size
+    if (dim_size // num_stacks_dim) < (MAX_SUBSTACK_SIZE[dim] / 100):
         # NOTE: We are just ignoring the user here
-        # We could default to MAX_TILE_SIZE[dim] / 100 as a lower bound
-        # But I think I'd rather just set our default tile size and ignore it!
-        return (dim_size // MAX_TILE_SIZE[dim]) + 1
-    # Make sure it's not too big, defined as 5x the max tile size
-    if (dim_size // num_tiles_dim) > (MAX_TILE_SIZE[dim] * 5):
+        # We could default to MAX_SUBSTACK_SIZE[dim] / 100 as a lower bound
+        # But I think I'd rather just set our default stacks size and ignore it!
+        return (dim_size // MAX_SUBSTACK_SIZE[dim]) + 1
+    # Make sure it's not too big, defined as 5x the max stacks size
+    if (dim_size // num_stacks_dim) > (MAX_SUBSTACK_SIZE[dim] * 5):
         # NOTE: Again we are just ignoring the user here
-        # We could default to MAX_TILE_SIZE[dim] * 5 as an upper bound...
-        return (dim_size // MAX_TILE_SIZE[dim]) + 1
-    return num_tiles_dim
+        # We could default to MAX_SUBSTACK_SIZE[dim] * 5 as an upper bound...
+        return (dim_size // MAX_SUBSTACK_SIZE[dim]) + 1
+    return num_stacks_dim
 
 
-def generate_patch_indices(
+def generate_stack_indices(
     image_shape: tuple[int, ...],
-    num_tiles: tuple[int, ...],
+    num_substacks: tuple[int, ...],
     overlap_fraction: tuple[float, ...],
 ) -> list[tuple[tuple[int, int], ...]]:
     """
-    Generate the indices for every patch for a given image size, desired number of tiles, and overlap fraction.
+    Generate the indices for every stack for a given image size, desired number of substacks, and overlap fraction.
 
-    Note that the overlap fraction is a float between 0 and 1, and the number of tiles is a tuple of integers, both of which should represent the same number of dimensions and meaning of the image_shape, which is expected to be a tuple of integers representing the dimensions of the image (e.g. (H, W, D)).
+    Note that the overlap fraction is a float between 0 and 1, and the number of substacks is a tuple of integers, both of which should represent the same number of dimensions and meaning of the image_shape, which is expected to be a tuple of integers representing the dimensions of the image (e.g. (H, W, D)).
 
-    Also note that the output is not guaranteed to completely satisfy the given arguments, as it may not be satisfiable. In this case, the overlap created will be different, but the number of tiles is guaranteed.
+    Also note that the output is not guaranteed to completely satisfy the given arguments, as it may not be satisfiable. In this case, the overlap created will be different, but the number of substacks is guaranteed.
     """
     # Extract the dimensions
     # TODO: Handle the case where the image is 2D
@@ -82,68 +85,68 @@ def generate_patch_indices(
     # TODO: Handle the image shape being e.g. D x H x W instead of H x W x D
     # In the above, we can probably assume D is minimum size
     height, width, depth = image_shape
-    num_tiles_height, num_tiles_width, num_tiles_depth = num_tiles
+    num_substacks_height, num_substacks_width, num_substacks_depth = num_substacks
     overlap_fraction_height, overlap_fraction_width, overlap_fraction_depth = (
         overlap_fraction
     )
     # The effective size of the image after multiply by the overlap added
-    # This helps create the appropriate patch size, amd we later create the overlap with offset
+    # This helps create the appropriate stack size, amd we later create the overlap with offset
     eff_height = round(height * (1 + overlap_fraction_height))
     eff_width = round(width * (1 + overlap_fraction_width))
     eff_depth = round(depth * (1 + overlap_fraction_depth))
 
-    # Calculate the patch size based on the number of tiles
-    patch_height = eff_height // num_tiles_height
-    patch_width = eff_width // num_tiles_width
-    patch_depth = eff_depth // num_tiles_depth
+    # Calculate the stack size based on the number of substacks
+    stack_height = eff_height // num_substacks_height
+    stack_width = eff_width // num_substacks_width
+    stack_depth = eff_depth // num_substacks_depth
 
     # Calculate overlap size based on fraction
     overlap_height = (
         0
         if overlap_fraction_height == 0
-        else max(int(patch_height * overlap_fraction_height), 1)
+        else max(int(stack_height * overlap_fraction_height), 1)
     )
     overlap_width = (
         0
         if overlap_fraction_width == 0
-        else max(int(patch_width * overlap_fraction_width), 1)
+        else max(int(stack_width * overlap_fraction_width), 1)
     )
     overlap_depth = (
         0
         if overlap_fraction_depth == 0
-        else max(int(patch_depth * overlap_fraction_depth), 1)
+        else max(int(stack_depth * overlap_fraction_depth), 1)
     )
 
-    # Generate indices for the patches
-    patch_indices = []
+    # Generate indices for the substacks
+    stack_indices = []
 
-    for i in range(num_tiles_height):
-        for j in range(num_tiles_width):
-            for k in range(num_tiles_depth):
-                start_h = i * (patch_height - overlap_height)
+    for i in range(num_substacks_height):
+        for j in range(num_substacks_width):
+            for k in range(num_substacks_depth):
+                start_h = i * (stack_height - overlap_height)
                 end_h = (
-                    min(start_h + patch_height, height)
-                    if i < max(num_tiles_height, 1) - 1
+                    min(start_h + stack_height, height)
+                    if i < max(num_substacks_height, 1) - 1
                     else height
                 )
-                start_w = j * (patch_width - overlap_width)
+                start_w = j * (stack_width - overlap_width)
                 end_w = (
-                    min(start_w + patch_width, width)
-                    if j < max(num_tiles_width, 1) - 1
+                    min(start_w + stack_width, width)
+                    if j < max(num_substacks_width, 1) - 1
                     else width
                 )
-                start_d = k * (patch_depth - overlap_depth)
+                start_d = k * (stack_depth - overlap_depth)
                 end_d = (
-                    min(start_d + patch_depth, depth)
-                    if k < max(num_tiles_depth, 1) - 1
+                    min(start_d + stack_depth, depth)
+                    if k < max(num_substacks_depth, 1) - 1
                     else depth
                 )
 
-                patch_indices.append(
+                stack_indices.append(
                     ((start_h, end_h), (start_w, end_w), (start_d, end_d))
                 )
 
-    return patch_indices, len(patch_indices)
+    return stack_indices, len(stack_indices)
 
 
 if __name__ == "__main__":
@@ -153,10 +156,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--img-csv", required=True, help="Path to csv file")
     parser.add_argument(
-        "--num-tiles",
+        "--num-substacks",
         required=True,
         nargs=3,
-        help="Number of tiles in each dimension (default is 'auto'). Assumed H x W x D.",
+        help="Number of stacks in each dimension (default is 'auto'). Assumed H x W x D.",
     )
     parser.add_argument(
         "--overlap",
@@ -169,7 +172,7 @@ if __name__ == "__main__":
         required=False,
         default="all_img_paths.csv",
         type=str,
-        help="Output csv file with patch indices",
+        help="Output csv file with stack indices",
     )
 
     args = parser.parse_args()
@@ -178,10 +181,10 @@ if __name__ == "__main__":
     img_csv_fpath = Path(args.img_csv)
     img_df = pd.read_csv(img_csv_fpath)
 
-    # Drop the patch info if it exists
+    # Drop the stack info if it exists
     img_df = img_df.drop(
         columns=[
-            "patch_idx",
+            "stack_idx",
             "start_h",
             "end_h",
             "start_w",
@@ -203,32 +206,32 @@ if __name__ == "__main__":
         img_shape = (row["height"], row["width"], row["num_slices"])
         # Ensure the image shape is a tuple of integers
         img_shape = tuple([int(val) for val in img_shape])
-        num_tiles = auto_tile_size(
+        num_substacks = auto_substack_size(
             img_shape,
-            args.num_tiles,
+            args.num_substacks,
         )
         # Ensure overlap is a tuple of floats
         overlap_fraction = tuple([float(val) for val in args.overlap])
 
-        # Generate the patch indices
-        patch_indices, _ = generate_patch_indices(
+        # Generate the stack indices
+        stack_indices, _ = generate_stack_indices(
             img_shape,
-            num_tiles=num_tiles,
+            num_substacks=num_substacks,
             overlap_fraction=overlap_fraction,
         )
 
-        for i, patch in enumerate(patch_indices):
+        for i, stack in enumerate(stack_indices):
             # Insert all info from the row
             for key, value in row.items():
                 new_csv[key].append(value)
-            # Add the patch info
-            new_csv["patch_idx"].append(i)
-            new_csv["start_h"].append(patch[0][0])
-            new_csv["end_h"].append(patch[0][1])
-            new_csv["start_w"].append(patch[1][0])
-            new_csv["end_w"].append(patch[1][1])
-            new_csv["start_d"].append(patch[2][0])
-            new_csv["end_d"].append(patch[2][1])
+            # Add the stack info
+            new_csv["stack_idx"].append(i)
+            new_csv["start_h"].append(stack[0][0])
+            new_csv["end_h"].append(stack[0][1])
+            new_csv["start_w"].append(stack[1][0])
+            new_csv["end_w"].append(stack[1][1])
+            new_csv["start_d"].append(stack[2][0])
+            new_csv["end_d"].append(stack[2][1])
 
     # Overwrite the csv with the new info
     new_csv_df = pd.DataFrame(new_csv)
