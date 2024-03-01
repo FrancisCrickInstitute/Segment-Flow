f9a6ed32128e11b7967cea924c77853432478dad
sea-shunned <seashunned@gmail.com>
2024-03-01 16:46:29 +0000

    Fix issue where overlap could create stacks above constraints, and refactor

--- modules/models/resources/usr/bin/create_splits.py
+++ modules/models/resources/usr/bin/create_splits.py
@@ -1,77 +1,107 @@
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from pathlib import Path
+import warnings
 
-import numpy as np
 import pandas as pd
 
+# TODO: Handle the case where the image is 2D (slice) or 3D (slice with channels)
+# TODO: Handle the case where the image is 4D (volume + channels)
+# TODO: Handle the case where the image is 5D (volume + time + channels)
+Stack = namedtuple("Stack", ["height", "width", "depth"])
 # TODO: These constants vary by GPU/available memory, and also scale by number of channels which is not considered here
 # Named substack to avoid patch/tile confusion, and to avoid chunk confusion
-MAX_SUBSTACK_SIZE = {
-    "height": 3000,
-    "width": 3000,
-    "depth": 100,
-}
+MAX_SUBSTACK_SIZE = Stack(
+    height=3000,
+    width=3000,
+    depth=100,
+)
 
 
-def auto_substack_size(image_shape: tuple[int, ...], num_substacks: tuple[int, ...]):
+def auto_size(size: int, max_size: int | float) -> int:
     """
-    Calculate the number of substacks to use for a given image size, for any dimension specified as 'auto'.
-
-    Assumes the image shape is a tuple of integers representing the dimensions of the image (D, H, W).
-
-    This uses the constants defined at the top of script to create chunks for each job within a given size, keeping
-    down the size (but increasing number) of the submitted jobs to avoid memory issues.
+    Calculate the number of stacks to use for a given size, based on a maximum size.
     """
-    # Extract the dimensions
-    depth, height, width = image_shape
-    # Extract the number of substacks
-    x_substacks, y_substacks, z_substacks = num_substacks
-    if x_substacks == "auto":
-        x_substacks = (height // MAX_SUBSTACK_SIZE["height"]) + 1
+    num_stacks = size // max_size
+    # We want to ensure that max_size is not exceeded
+    if (size / num_stacks) > max_size:
+        num_stacks += 1
+    return int(num_stacks)
+
+
+def calc_num_stacks_dim(
+    dim_size: int, req_stacks_dim: int, overlap: float, dim: str
+) -> tuple[int, int]:
+    """
+    Determine the number of stacks to use for a given dimension, based on the size of the dimension, the number of stacks requested, the overlap fraction requested, and our maximum size constraints set at the top.
+    """
+    # Calculate the effective size of the image after multiplying by the added overlap
+    # This is ignored/irrelevant if only 1 stack is requested or if the overlap is 0 respectively
+    eff_size = round(dim_size * (1 + overlap))
+    if req_stacks_dim == "auto":
+        num_stacks_dim = auto_size(eff_size, getattr(MAX_SUBSTACK_SIZE, dim))
+        if num_stacks_dim == 1 and overlap > 0:
+            warnings.warn(
+                f"'Auto' determined only 1 stack is needed, but overlap is set to {overlap}. This will result in overlap being ignored."
+            )
     else:
-        x_substacks = check_sensible_num_substacks(
-            int(x_substacks), height, dim="height"
-        )
-    if y_substacks == "auto":
-        y_substacks = (width // MAX_SUBSTACK_SIZE["width"]) + 1
-    else:
-        y_substacks = check_sensible_num_substacks(int(y_substacks), width, dim="width")
-    if z_substacks == "auto":
-        z_substacks = (depth // MAX_SUBSTACK_SIZE["depth"]) + 1
-    else:
-        z_substacks = check_sensible_num_substacks(int(z_substacks), depth, dim="depth")
-    return (x_substacks, y_substacks, z_substacks)
-
-
-def check_sensible_num_substacks(num_stacks_dim: int, dim_size: int, dim: str):
-    """
-    Make sure the input number of substacks is sensible for the given dimension size.
-    """
-    # Make sure the number of stacks is at least 1
+        num_stacks_dim = int(req_stacks_dim)
+    # Check that whatever num_stacks_dim set is sensible
+    # Ignore zero or negative numbers
     if num_stacks_dim < 1:
         num_stacks_dim = 1
     # More stacks than pixels? Use auto method
-    if num_stacks_dim > dim_size:
-        return (dim_size // MAX_SUBSTACK_SIZE[dim]) + 1
+    if num_stacks_dim > eff_size:
+        num_stacks_dim = auto_size(eff_size, getattr(MAX_SUBSTACK_SIZE, dim))
     # Make sure it's not too small, defined as 1% of the max stacks size
-    if (dim_size // num_stacks_dim) < (MAX_SUBSTACK_SIZE[dim] / 100):
+    elif (eff_size // num_stacks_dim) < (getattr(MAX_SUBSTACK_SIZE, dim) / 100):
         # NOTE: We are just ignoring the user here
-        # We could default to MAX_SUBSTACK_SIZE[dim] / 100 as a lower bound
+        # We could default to getattr(MAX_SUBSTACK_SIZE, dim) / 100 as a lower bound
         # But I think I'd rather just set our default stacks size and ignore it!
-        return (dim_size // MAX_SUBSTACK_SIZE[dim]) + 1
+        num_stacks_dim = auto_size(eff_size, getattr(MAX_SUBSTACK_SIZE, dim))
     # Make sure it's not too big, defined as 5x the max stacks size
-    if (dim_size // num_stacks_dim) > (MAX_SUBSTACK_SIZE[dim] * 5):
+    elif (eff_size // num_stacks_dim) > (getattr(MAX_SUBSTACK_SIZE, dim) * 5):
         # NOTE: Again we are just ignoring the user here
-        # We could default to MAX_SUBSTACK_SIZE[dim] * 5 as an upper bound...
-        return (dim_size // MAX_SUBSTACK_SIZE[dim]) + 1
-    return num_stacks_dim
+        # We could default to getattr(MAX_SUBSTACK_SIZE, dim) * 5 as an upper bound...
+        num_stacks_dim = auto_size(eff_size, getattr(MAX_SUBSTACK_SIZE, dim))
+    # If num_stacks_dim is 1, and overlap is >0, warn the user
+    if num_stacks_dim == 1 and overlap > 0:
+        warnings.warn(
+            f"You asked for 1 {dim} stack, but overlap is set to {overlap}. This will result in overlap being ignored."
+        )
+    return num_stacks_dim, eff_size
+
+
+def calc_num_stacks(
+    img_shape: Stack, req_stacks: Stack, overlap_fraction: Stack
+) -> tuple[Stack, Stack]:
+    num_stacks_height, eff_height = calc_num_stacks_dim(
+        img_shape.height, req_stacks.height, overlap_fraction.height, "height"
+    )
+    num_stacks_width, eff_width = calc_num_stacks_dim(
+        img_shape.width, req_stacks.width, overlap_fraction.width, "width"
+    )
+    num_stacks_depth, eff_depth = calc_num_stacks_dim(
+        img_shape.depth, req_stacks.depth, overlap_fraction.depth, "depth"
+    )
+    num_stacks = Stack(
+        height=num_stacks_height,
+        width=num_stacks_width,
+        depth=num_stacks_depth,
+    )
+    eff_shape = Stack(
+        height=eff_height,
+        width=eff_width,
+        depth=eff_depth,
+    )
+    return num_stacks, eff_shape
 
 
 def generate_stack_indices(
-    image_shape: tuple[int, ...],
-    num_substacks: tuple[int, ...],
-    overlap_fraction: tuple[float, ...],
-) -> list[tuple[tuple[int, int], ...]]:
+    image_shape: Stack,
+    num_substacks: Stack,
+    overlap_fraction: Stack,
+    eff_shape: Stack,
+) -> tuple[list[tuple[tuple[int, int], ...]], int, Stack]:
     """
     Generate the indices for every stack for a given image size, desired number of substacks, and overlap fraction.
 
@@ -79,79 +109,51 @@ def generate_stack_indices(
 
     Also note that the output is not guaranteed to completely satisfy the given arguments, as it may not be satisfiable. In this case, the overlap created will be different, but the number of substacks is guaranteed.
     """
-    # Extract the dimensions
-    # TODO: Handle the case where the image is 2D
-    # TODO: Handle the case where the image is 4D
-    # TODO: Handle the image shape being e.g. H x W x D instead of D x H x W
-    # In the above, we can probably assume D is minimum size
-    depth, height, width = image_shape
-    num_substacks_height, num_substacks_width, num_substacks_depth = num_substacks
-    overlap_fraction_height, overlap_fraction_width, overlap_fraction_depth = (
-        overlap_fraction
-    )
-    # The effective size of the image after multiply by the overlap added
-    # This helps create the appropriate stack size, amd we later create the overlap with offset
-    # This only counts if we have more than 1 substack in that dimension
-    eff_height = (
-        round(height * (1 + overlap_fraction_height))
-        if num_substacks_height > 1
-        else height
-    )
-    eff_width = (
-        round(width * (1 + overlap_fraction_width))
-        if num_substacks_width > 1
-        else width
-    )
-    eff_depth = (
-        round(depth * (1 + overlap_fraction_depth))
-        if num_substacks_depth > 1
-        else depth
-    )
     # Calculate the stack size based on the number of substacks
-    stack_height = eff_height // num_substacks_height
-    stack_width = eff_width // num_substacks_width
-    stack_depth = eff_depth // num_substacks_depth
+    stack_height = eff_shape.height // num_substacks.height
+    stack_width = eff_shape.width // num_substacks.width
+    stack_depth = eff_shape.depth // num_substacks.depth
 
     # Calculate overlap size based on fraction
     overlap_height = (
         0
-        if overlap_fraction_height == 0
-        else max(int(stack_height * overlap_fraction_height), 1)
+        if overlap_fraction.height == 0
+        else max(int(stack_height * overlap_fraction.height), 1)
     )
     overlap_width = (
         0
-        if overlap_fraction_width == 0
-        else max(int(stack_width * overlap_fraction_width), 1)
+        if overlap_fraction.width == 0
+        else max(int(stack_width * overlap_fraction.width), 1)
     )
     overlap_depth = (
         0
-        if overlap_fraction_depth == 0
-        else max(int(stack_depth * overlap_fraction_depth), 1)
+        if overlap_fraction.depth == 0
+        else max(int(stack_depth * overlap_fraction.depth), 1)
     )
 
     # Generate indices for the substacks
     stack_indices = []
 
-    for i in range(num_substacks_height):
-        for j in range(num_substacks_width):
-            for k in range(num_substacks_depth):
+    for i in range(num_substacks.height):
+        for j in range(num_substacks.width):
+            for k in range(num_substacks.depth):
                 start_h = i * (stack_height - overlap_height)
                 end_h = (
-                    min(start_h + stack_height, height)
-                    if i < max(num_substacks_height, 1) - 1
-                    else height
+                    min(start_h + stack_height, image_shape.height)
+                    if i < max(num_substacks.height, 1) - 1
+                    else image_shape.height
                 )
                 start_w = j * (stack_width - overlap_width)
                 end_w = (
-                    min(start_w + stack_width, width)
-                    if j < max(num_substacks_width, 1) - 1
-                    else width
+                    min(start_w + stack_width, image_shape.width)
+                    if j < max(num_substacks.width, 1) - 1
+                    else image_shape.width
                 )
                 start_d = k * (stack_depth - overlap_depth)
                 end_d = (
-                    min(start_d + stack_depth, depth)
-                    if k < max(num_substacks_depth, 1) - 1
-                    else depth
+                    min(start_d + stack_depth, image_shape.depth)
+                    if k < max(num_substacks.depth, 1) - 1
+                    else image_shape.depth
                 )
 
                 stack_indices.append(
@@ -160,7 +162,7 @@ def generate_stack_indices(
     return (
         stack_indices,
         len(stack_indices),
-        (stack_depth, stack_height, stack_width),
+        Stack(stack_height, stack_width, stack_depth),
     )
 
 
@@ -232,21 +234,25 @@ if __name__ == "__main__":
     for idx, row in img_df.iterrows():
         img_path = Path(row["img_path"])
         # Extract the image shape from the row
-        img_shape = (row["num_slices"], row["height"], row["width"])
-        # Ensure the image shape is a tuple of integers
-        img_shape = tuple([int(val) for val in img_shape])
-        num_substacks = auto_substack_size(
-            img_shape,
-            args.num_substacks,
+        img_shape = Stack(
+            height=int(row["height"]),
+            width=int(row["width"]),
+            depth=int(row["num_slices"]),
         )
+        # Get the requested number of substacks (either int or 'auto' for each dimension)
+        num_substacks = Stack(*args.num_substacks)
         # Ensure overlap is a tuple of floats
-        overlap_fraction = tuple([float(val) for val in args.overlap])
-
+        overlap_fraction = Stack(*map(float, args.overlap))
+        # Calculate the number of stacks and the effective shape
+        num_substacks, eff_shape = calc_num_stacks(
+            img_shape, num_substacks, overlap_fraction
+        )
         # Generate the stack indices
         stack_indices, num_stacks, stack_size = generate_stack_indices(
-            img_shape,
+            image_shape=img_shape,
             num_substacks=num_substacks,
             overlap_fraction=overlap_fraction,
+            eff_shape=eff_shape,
         )
 
         for i, stack in enumerate(stack_indices):
