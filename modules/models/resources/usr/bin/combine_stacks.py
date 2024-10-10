41b81b80bba02390145ff3488838cef3caedb97b
sea-shunned <seashunned@gmail.com>
2024-10-10 17:01:50 +0100

    Ensure reduce_dtype always called in combination

--- modules/models/resources/usr/bin/combine_stacks.py
+++ modules/models/resources/usr/bin/combine_stacks.py
@@ -344,6 +344,10 @@ if __name__ == "__main__":
                 )
         else:
             combined_masks = connect_components(combined_masks)
+    # Ensure the dtype is always reduced if possible
+    # NOTE: Postprocessing funcs above handle this themselves
+    else:
+        combined_masks = reduce_dtype(combined_masks)
     mem_used = psutil.Process(os.getpid()).memory_info().rss / (1024.0**3)
     print(f"Memory used in combination: {mem_used:.2f} GB")
     # Save the masks
