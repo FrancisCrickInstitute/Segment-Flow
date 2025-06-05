34b710b36db1da90300ffdfbdaaa9764271f43ff
sea-shunned <seashunned@gmail.com>
2025-06-05 13:16:51 +0100

    Add cellpose-sam script and envs

--- modules/models/resources/usr/bin/run_cellposesam.py
+++ modules/models/resources/usr/bin/run_cellposesam.py
@@ -13,22 +13,29 @@ def run_cellpose(
     save_name: str,
     idxs: list[int, ...],
     config: dict,
+    model_chkpt: str,
 ):
+    # NOTE: This is just a link to the Cellpose-SAM model, but circumvents Cellpose's fixed model location
+    # Initialize Cellpose-SAM model
+    cp_model = models.CellposeModel(
+        gpu=get_device().type == "cuda",
+        pretrained_model=str(Path(model_chkpt).readlink()),
+    )
     # Extract model config arguments
-    masks, _, _, _ = cp_model.eval(
+    # NOTE: rescale and channels not used anymore
+    # NOTE: more than 3 channels not used
+    masks, _, _ = cp_model.eval(
         img,
-        diameter=config["diameter"],
-        channels=config["channels"],
         batch_size=config["batch_size"],
-        channel_axis=config["channel_axis"],
+        flow_threshold=config["flow_threshold"],
+        cellprob_threshold=config["cellprob_threshold"],
         z_axis=config["z_axis"],
+        channel_axis=config["channel_axis"],
         do_3D=config["do_3D"],
         anisotropy=config["anisotropy"],
         stitch_threshold=config["stitch_threshold"],
-        cellprob_threshold=config["cellprob_threshold"],
-        flow_threshold=config["flow_threshold"],
-        niter=config["niter"],
         min_size=config["min_size"],
+        max_size_fraction=config["max_size_fraction"],
     )
 
     save_masks(Path(save_dir), save_name, masks, idxs=idxs, mask_type="instance")
@@ -37,7 +44,6 @@ def run_cellpose(
 if __name__ == "__main__":
     parser = create_argparser_inference()
     cli_args = parser.parse_args()
-
     with open(cli_args.model_config, "r") as f:
         config = yaml.safe_load(f)
 
@@ -63,21 +69,14 @@ if __name__ == "__main__":
         config["z_axis"] = 1
     else:
         config["z_axis"] = None
-
+    # Ensure 3D properly set
+    # TODO: Does Cellpose-SAM still need this or does it better handle 3D in 2D mode?
     config["do_3D"] = True if cli_args.num_slices > 1 else False
-
-    # Extract the segment and nucleus channels
-    config["channels"] = [
-        config["segment_channel"],
-        config["nucleus_channel"],
-    ]
-
-    # Create the Cellpose model with available device
-    cp_model = models.Cellpose(model_type=cli_args.model_type, device=get_device())
 
     run_cellpose(
         save_dir=cli_args.output_dir,
         save_name=cli_args.mask_fname,
         idxs=cli_args.idxs,
         config=config,
+        model_chkpt=cli_args.model_chkpt,
     )
